db6ef60698755725af453f168bf2472b4f669239
goodtam8 <21225133@life.hkbu.edu.hk>
2024-07-04 15:13:48 +0800

    modify the backend route and turn it into ajax form

--- fyp/routes/courses.js
+++ fyp/routes/courses.js
@@ -11,8 +11,13 @@ const { connectToDB, ObjectId } = require('../utils/db');
 router.post('/', async function (req, res) {
     const db = await connectToDB();
     try {
-        console.log(req.semester)
-       
+
+        req.body.year = parseInt(req.body.year);
+        req.body.semester = parseInt(req.body.semester);
+        req.body.quota = parseInt(req.body.quota);
+
+
+
  
         let result = await db.collection("course").insertOne(req.body);
         res.status(201).json({ id: result.insertedId });
@@ -23,5 +28,23 @@ router.post('/', async function (req, res) {
     }
   });
 
+
+
+  /* Retrieve a single course by using cid to search */
+router.get('/:id', async function (req, res) {
+    const db = await connectToDB();
+    try {
+        let result = await db.collection("course").findOne({ cid: req.params.id });
+        if (result) {
+            res.json(result);   
+        } else {
+            res.status(404).json({ message: "Booking not found" });
+        }
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    } finally {
+        await db.client.close();
+    }
+});
 
   module.exports = router;
