7c17ab2d213d02df3f21633873ad6057b30d5f38
goodtam8 <21225133@life.hkbu.edu.hk>
2024-07-05 10:03:24 +0800

    implement the backend update and delete route for the system

--- fyp/routes/courses.js
+++ fyp/routes/courses.js
@@ -28,7 +28,23 @@ router.post('/', async function (req, res) {
     }
   });
 
+// Delete a single course
+router.delete('/:id', async function (req, res) {
+    const db = await connectToDB();
+    try {
+        let result = await db.collection("course").deleteOne({ _id: new ObjectId(req.params.id) });
 
+        if (result.deletedCount > 0) {
+            res.status(200).json({ message: "Course deleted" });
+        } else {
+            res.status(404).json({ message: "Course not found" });
+        }
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    } finally {
+        await db.client.close();
+    }
+});
 
   /* Retrieve a single course by using cid to search */
 router.get('/:id', async function (req, res) {
@@ -38,7 +54,7 @@ router.get('/:id', async function (req, res) {
         if (result) {
             res.json(result);   
         } else {
-            res.status(404).json({ message: "Booking not found" });
+            res.status(404).json({ message: "Course not found" });
         }
     } catch (err) {
         res.status(400).json({ message: err.message });
@@ -47,6 +63,23 @@ router.get('/:id', async function (req, res) {
     }
 });
 
+
+/* Retrieve a single Booking */
+router.get('/course/:id', async function (req, res) {
+    const db = await connectToDB();
+    try {
+        let result = await db.collection("course").findOne({ _id: new ObjectId(req.params.id) });
+        if (result) {
+            res.json(result);   
+        } else {
+            res.status(404).json({ message: "Course not found" });
+        }
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    } finally {
+        await db.client.close();
+    }
+});
 
 router.get('/', async function (req, res) {
     const db = await connectToDB();
@@ -70,6 +103,29 @@ router.get('/', async function (req, res) {
         res.status(400).json({ message: err.message });
     }
     finally {
+        await db.client.close();
+    }
+});
+
+// Update a single Booking
+router.put('/:id', async function (req, res) {
+    const db = await connectToDB();
+    try {
+        req.body.numTickets = parseInt(req.body.numTickets);
+        req.body.terms = req.body.terms == "on";
+        req.body.superhero = req.body.superhero || "";
+        req.body.modifiedAt = new Date();
+
+        let result = await db.collection("course").updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body });
+
+        if (result.modifiedCount > 0) {
+            res.status(200).json({ message: "Course updated" });
+        } else {
+            res.status(404).json({ message: "Course not found" });
+        }
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    } finally {
         await db.client.close();
     }
 });
