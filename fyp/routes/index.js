1e4989a7159b0428117709c57e2b3fefd0931e41
goodtam8 <21225133@life.hkbu.edu.hk>
2024-08-07 17:25:45 +0800

    implement login in student and teacher

--- fyp/routes/index.js
+++ fyp/routes/index.js
@@ -13,14 +13,68 @@ router.post('/api/login', async function (req, res, next) {
     // check if the user exists
     var user = await db.collection("users").findOne({ email: req.body.email });
     if (!user) {
-      res.status(401).json({ message: 'User not found' });
+      res.status(401).json({ message: 'Admin not found' });
       return;
     }
 
 // res.json(user);
 
 delete user.password;
-delete user.ip_address;
+
+// generate a JWT token
+const token = generateToken(user);
+
+// return the token
+res.json({ token: token });    
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  } finally {
+    await db.client.close();
+  }
+});
+
+router.post('/api/login/student', async function (req, res, next) {
+  const db = await connectToDB();
+  try {
+    let id = parseInt(req.body.sid)
+    // check if the user exists
+    var user = await db.collection("student").findOne({ sid:id });
+    if (!user) {
+      res.status(401).json({ message: 'Admin not found' });
+      return;
+    }
+
+// res.json(user);
+
+delete user.password;
+
+// generate a JWT token
+const token = generateToken(user);
+
+// return the token
+res.json({ token: token });    
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  } finally {
+    await db.client.close();
+  }
+});
+
+router.post('/api/login/teacher', async function (req, res, next) {
+  const db = await connectToDB();
+  try {
+    let id = parseInt(req.body.sid)
+
+    // check if the user exists
+    var user = await db.collection("teacher").findOne({ staff_id: id});
+    if (!user) {
+      res.status(401).json({ message: 'Admin not found' });
+      return;
+    }
+
+// res.json(user);
+
+delete user.password;
 
 // generate a JWT token
 const token = generateToken(user);
