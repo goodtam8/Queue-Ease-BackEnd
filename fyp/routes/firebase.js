273b982717fb24530f834bdf6a44039472555602
Sun Tam <21225133@life.hkbu.edu.hk>
2025-05-08 10:23:11 +0800

    update the security issue

--- fyp/routes/firebase.js
+++ fyp/routes/firebase.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 const admin = require('firebase-admin');
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
